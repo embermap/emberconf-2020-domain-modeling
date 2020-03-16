758b9958c98058257a9a12a97b4067d1c2a6721e
Sam Selikoff <sam.selikoff@gmail.com>
2020-03-15 20:16:34 -0400

    method selector

--- app/components/inspector/request/component.js
+++ app/components/inspector/request/component.js
@@ -3,12 +3,21 @@ import { tracked } from "@glimmer/tracking";
 import { action } from "@ember/object";
 
 export default class extends Component {
+  @tracked method = "GET";
   @tracked endpoint = "/api/messages";
+
+  @action
+  updateMethod(e) {
+    this.method = e.target.value;
+  }
 
   @action
   makeRequest(e) {
     e.preventDefault();
 
-    fetch(this.endpoint);
+    fetch(this.endpoint, {
+      method: this.method,
+      body: this.requestBody
+    });
   }
 }
